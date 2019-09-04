c95291f66fd83dd0942e0ec6a754992cdbabd0da
Michael Gellings <michael.gellings@gmx.net>
2019-09-04 12:50:52 +0200

    add dynamic alignment, store settings in localStorage

--- components/form.js
+++ components/form.js
@@ -2,7 +2,8 @@ import { getText, setText, getAlign, setAlign } from "./meme.js";
 import { inputToOutput } from "./canvas.js";
 
 const meme_text_01 = document.querySelector("#input_text_01");
-const meme_aling_01 = document.querySelector("align_01");
+// const meme_align_01 = document.querySelector("input[name='align_01']:checked");
+const meme_align_01 = document.querySelectorAll("input[type='radio']");
 
 export function addEventListeners() {
   meme_text_01.placeholder = getText();
@@ -10,6 +11,15 @@ export function addEventListeners() {
     setText(event.target.value);
     inputToOutput();
   });
+  if (meme_align_01) {
+    meme_align_01.forEach(function(radioButton) {
+      radioButton.addEventListener("click", function(event) {
+        setAlign(event.target.value);
+        // console.log(event.target.value);
+        inputToOutput();
+      });
+    });
+  }
 
-  meme_aling_01.value = getAlign();
+  // meme_align_01.value = getAlign();
 }
